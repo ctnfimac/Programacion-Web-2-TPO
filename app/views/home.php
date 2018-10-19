5bba51b134aca9d2f404d9445585bdaf2aa85aab
Christian Peralta <christianipc.1987@gmail.com>
2018-10-19 03:07:17 -0300

    creacion de la carpeta public la cual contiene todos los css, js, img , fonts. completo la vista home y creo carpeta llamada  home la que contendra las diferentes partes del home como banner, servicios, contacto etc

--- app/views/home.php
+++ app/views/home.php
@@ -1,3 +1,22 @@
 <?php
 
-echo 'estas en el home';
+// echo 'estas en el home';
+include('home/overall/head.php');
+
+require_once('home/sections/nav.php');
+require_once('home/sections/banner.php');
+require_once('home/sections/servicios.php');
+require_once('home/sections/promocion.php');
+require_once('home/sections/galeria.php');
+require_once('home/sections/contacto.php');
+require_once('home/sections/modal_registro.php');
+
+printf($nav);
+printf($banner);
+printf($servicios);
+printf($promocion);
+printf($galeria);
+printf($contacto);
+printf($modal_registro);
+
+include('home/overall/footer.php');
---
+++ app/views/home/sections/galeria.php
@@ -0,0 +1,86 @@
+<?php
+
+$galeria = '
+<section id="galeria" class="galeria mt-5 mb-5">
+	<h2 class="text-center">Menus</h2>
+	<p class="text-center">Mire alguno de nuestros diferentes deliciosos menus</p>
+	<div class="container-fluid">
+		<div class="row">
+		<div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="./public/img/home/galeria/galeria01.jpg" alt="pizza">
+					<div class="card-body">
+					<h4 class="card-title float-left pt-3"><a>Pizza de Muzzarella</a></h4>
+					<span class="\text-info float-right p-3 block-example border border-warning\">$250</span>
+					</div>
+				</div>
+			</div>
+		<div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="./public/img/home/galeria/galeria01.jpg" alt="pizza">
+					<div class="card-body">
+					<h4 class="card-title float-left pt-3"><a>Pizza de Muzzarella</a></h4>
+					<span class="text-info float-right p-3 block-example border border-warning">$250</span>
+					</div>
+				</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="./public/img/home/galeria/galeria01.jpg" alt="pizza">
+					<div class="card-body">
+						<h4 class="card-title float-left pt-3"><a>Pizza de Muzzarella</a></h4>
+						<span class="text-info float-right p-3 block-example border border-warning">$250</span>
+					</div>
+				</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="./public/img/home/galeria/galeria01.jpg" alt="pizza">
+					<div class="card-body">
+						<h4 class="card-title float-left pt-3"><a>Pizza de Muzzarella</a></h4>
+						<span class="text-info float-right p-3 block-example border border-warning">$250</span>
+					</div>
+				</div>
+		</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="./public/img/home/galeria/galeria01.jpg" alt="pizza">
+					<div class="card-body">
+						<h4 class="card-title float-left pt-3"><a>Pizza de Muzzarella</a></h4>
+						<span class="text-info float-right p-3 block-example border border-warning">$250</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="./public/img/home/galeria/galeria01.jpg" alt="pizza">
+					<div class="card-body">
+						<h4 class="card-title float-left pt-3"><a>Pizza de Muzzarella</a></h4>
+						<span class="text-info float-right p-3 block-example border border-warning">$250</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="./public/img/home/galeria/galeria01.jpg" alt="pizza">
+					<div class="card-body">
+						<h4 class="card-title float-left pt-3"><a>Pizza de Muzzarella</a></h4>
+						<span class="text-info float-right p-3 block-example border border-warning">$250</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="./public/img/home/galeria/galeria01.jpg" alt="pizza">
+					<div class="card-body">
+						<h4 class="card-title float-left pt-3"><a>Pizza de Muzzarella</a></h4>
+						<span class="text-info float-right p-3 block-example border border-warning">$250</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section> 
+';
