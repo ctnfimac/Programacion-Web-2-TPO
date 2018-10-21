66665269f0b15f11052f7c924b0f5e3cd42fc1e2
Alexander Prada <aleprada_ar@hotmail.com>
2018-10-21 20:04:52 -0300

    Creada tabla admin y agregados usuarios

--- base_de_datos/sistema_pedidos.sql
+++ base_de_datos/sistema_pedidos.sql
@@ -31,6 +31,8 @@ create table Cliente (
 	calle varchar(30) not null,
 	numero varchar(30) not null,
 	id_localidad int,
+    id_usuario int not null,
+    CONSTRAINT FK_CLIENTE_USER FOREIGN KEY (id_usuario) REFERENCES Usuario(id),
 	CONSTRAINT FK_USUARIO_LOCALIDAD FOREIGN KEY (id_localidad) REFERENCES Localidad(id)
 );
 
@@ -38,7 +40,9 @@ create table Repartidor (
 	id int primary key,
 	fecha_nacimiento date not null,
 	dni varchar(8) not null,
-	cuil varchar(11) not null
+	cuil varchar(11) not null,
+    id_usuario int not null,
+    CONSTRAINT FK_REPARTIDOR_USER FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
 create table Comercio (
@@ -46,12 +50,18 @@ create table Comercio (
 	cuit varchar(11) not null
 );
 
-create table Admin (
+create table Gerente (
 	id int primary key,
 	id_usuario int not null,
 	id_comercio int not null,
-	CONSTRAINT FK_ADMIN_USER FOREIGN KEY (id_usuario) REFERENCES Usuario(id),
+	CONSTRAINT FK_GERENTE_USER FOREIGN KEY (id_usuario) REFERENCES Usuario(id),
 	CONSTRAINT FK_COMERCIO_ADMIN FOREIGN KEY (id_comercio) REFERENCES Comercio(id)
+);
+
+create table Admin (
+	id int primary key,
+	id_usuario int not null,
+	CONSTRAINT FK_ADMIN_USER FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
 create table Sucursal (
@@ -112,4 +122,13 @@ create table Oferta (
 	CONSTRAINT FK_MENU_OFERTA FOREIGN KEY (id_menu) REFERENCES Menu(id)
 );
 
+insert into Usuario(id, nombre, apellido, email, contrasenia) values
+(1, 'Alexander', 'Prada', 'notengoemail@gmail.com', 'alex123'),
+(2, 'Christian', 'Peralta', 'notengoemail2@gmail.com', 'christian123'),
+(3, 'Martin', 'Garra', 'notengoemail3@gmail.com', 'martin123');
+
+insert into Admin(id, id_usuario) values
+(1, 1),
+(2, 2),
+(3, 3);
 
